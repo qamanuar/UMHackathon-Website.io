52dc9a9c241a6350d5346755668d0fe563a915b9
Qamarul <anuarq999@gmail.com>
2025-04-06 23:20:57 +0800

    fix pt3

--- firebase-config.js
+++ firebase-config.js
@@ -1,31 +1,11 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
 
-import * as dotenv from 'dotenv';
+import { firebaseConfig } from './firebase-keys.js';
 
-dotenv.config();
-// Import the Firebase configuration from firebase-keys.js
-// import firebaseConfig from './firebase-keys.js';
-
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY, 
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
-  };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);                // Firebase Authentication
-const db = getFirestore(app);             // Firestore Database
-
-// console.log('Firebase app initialized:', app);
-// console.log('Firebase auth initialized:', auth);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 export { app, auth, db };
