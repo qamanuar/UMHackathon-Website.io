8942f6ce2871abbfc0b7df5909f6105c0d8ebe08
Qamarul <anuarq999@gmail.com>
2025-04-06 22:02:27 +0800

    fix pt 3

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
-import { getFirestore } from "firebase/firestore";
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
+
 import * as dotenv from 'dotenv';
 
 dotenv.config();
