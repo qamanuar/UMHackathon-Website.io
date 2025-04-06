5f6f4c9ffd98f6edc4f075db7da93c3aafa8b40b
Qamarul <anuarq999@gmail.com>
2025-04-06 23:41:03 +0800

    Fixed firebase config import

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
 
-import { firebaseConfig } from './firebase-keys.js';
+import firebaseConfig from './firebase-keys.js';
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
--- firebase-keys.js
+++ firebase-keys.js
@@ -1,4 +1,4 @@
-export const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyCA1JcMr1qEw_ioOSaQfDSNMc4VhBms-xQ",
     authDomain: "umhackathon2025.firebaseapp.com",
     databaseURL: "https://umhackathon2025-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -7,4 +7,6 @@ export const firebaseConfig = {
     messagingSenderId: "856480389899",
     appId: "1:856480389899:web:b02cce20128e274962e00e",
     measurementId: "G-Z7P123YH56"
-  };
+};
+
+export default firebaseConfig;
